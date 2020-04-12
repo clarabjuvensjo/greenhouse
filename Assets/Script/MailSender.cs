a380ca25750eab8915b2b5066ebf3e6802d0436f
Clara Bjuvensjö <clara.bjuvensjo@gmail.com>
2020-04-12 14:49:24 +0200

    Initial commit

---
+++ Assets/Script/MailSender.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+
+public class MailSender : MonoBehaviour
+{
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button.GetComponent<Button>().onClick.AddListener(SendEmail);
+    }
+
+    public void SendEmail()
+    {
+        string email = "MY EMAIL ADDRESS";
+        string subject = MyEscapeURL("My Subject");
+        string body = MyEscapeURL("My Body\r\nFull of non-escaped chars");
+
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+    }
+    string MyEscapeURL(string URL)
+    {
+        return WWW.EscapeURL(URL).Replace("+", "%20");
+    }
+}
+
---
+++ Assets/Script/StartScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // user is touching the screen with a single touch 
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
+
+

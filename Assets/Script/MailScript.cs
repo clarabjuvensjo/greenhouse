9eba4ea98370e15c7d821260cf7c15d84057f3df
Clara Bjuvensjö <clara.bjuvensjo@gmail.com>
2020-04-28 14:22:27 +0200

    Initial commit

---
+++ Assets/Script/MailScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MailScript : MonoBehaviour
+{
+    public void changeScene(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
